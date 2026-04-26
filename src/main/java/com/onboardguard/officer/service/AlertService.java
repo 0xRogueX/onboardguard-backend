3a52e056b5ec710bbf5795f90dc96b1939bcfea1
vrundatss <vrunda.chavada@tssconsultancy.com>
2026-04-26 12:54:18 +0530

    feat(officer) : implemented Alert , case and CaseNote  service

---
+++ src/main/java/com/onboardguard/officer/service/AlertService.java
@@ -0,0 +1,13 @@
+package com.onboardguard.officer.service;
+
+import com.onboardguard.officer.dto.AlertDetailDto;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface AlertService {
+
+    AlertDetailDto acknowledgeAlert(Long alertId, Long officerId);
+
+    void dismissAlert(Long alertId, Long officerId, String reason);
+
+    Long convertToCase(Long alertId, Long officerId);
+}
---
+++ src/main/java/com/onboardguard/officer/service/CaseNoteService.java
@@ -0,0 +1,4 @@
+package com.onboardguard.officer.service;
+
+public interface CaseNoteService {
+}
