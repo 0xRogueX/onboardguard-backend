7067523038d56fbfd11b40b28b6687ac13dec69e
0xRogueX <dev.vivek.dadhaniya@gmail.com>
2026-05-02 17:08:21 +0530

    feat: add the pageable for get all watchlist entries

--- src/main/java/com/onboardguard/watchlist/service/WatchlistService.java
+++ src/main/java/com/onboardguard/watchlist/service/WatchlistService.java
@@ -1,5 +1,6 @@
 package com.onboardguard.watchlist.service;
 
+import com.onboardguard.shared.common.enums.CategoryCode;
 import com.onboardguard.watchlist.dto.WatchlistCategoryDto;
 import com.onboardguard.watchlist.dto.WatchlistEntryResponseDto;
 import org.springframework.data.domain.Page;
@@ -8,7 +9,8 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface WatchlistService {
-    Page<WatchlistEntryResponseDto> getAllActiveEntries(com.onboardguard.shared.common.enums.CategoryCode categoryCode, Pageable pageable);
+
+    Page<WatchlistEntryResponseDto> getAllActiveEntries(CategoryCode categoryCode, Pageable pageable);
 
     WatchlistEntryResponseDto getEntryDetails(Long entryId);
 
